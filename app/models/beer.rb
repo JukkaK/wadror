d9793eacab93629155ec900c0d2f7ee8b3fbe5e4
Jukka Koskelin <jukka.koskelin@helsinki.fi>
2014-01-22 15:01:26 +0200

    toisen viikon tehtavia lisaa

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,11 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    #return ratings.average('score')
+    return ratings.inject(0) {|x,y| x+y.score}/ratings.count
+
+  end
+
 end
