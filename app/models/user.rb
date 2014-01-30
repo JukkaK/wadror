4afbca90f32ba34afffa9041df78f76d16ed83a2
Jukka Koskelin <jukka.koskelin@helsinki.fi>
2014-01-30 13:46:01 +0200

    lisatty tiivisste

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
   include RatingAverage
 
+  has_secure_password
+
   validates :username, uniqueness: true,
             length: { minimum: 3 },
             length: { maximum: 15}
 
   has_many :ratings   # käyttäjällä on monta ratingia
+  has_many :beers, through: :ratings
 end
