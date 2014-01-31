5e495331148f56a60833156d6a5d13ed4d8ef13a
Jukka Koskelin <jukka.koskelin@helsinki.fi>
2014-01-31 14:06:42 +0200

    tehtava 6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,12 +3,10 @@ class Brewery < ActiveRecord::Base
   has_many :beers, :dependent => :destroy
   has_many :ratings, :through => :beers
 
-  validates :year, numericality: true
-  validates_inclusion_of :year, in: 1042..2014
   validates :name, presence: true
+  validate :validate_year_format, :validate_start_year, :validate_end_year
 
-
-            def print_report
+  def print_report
     puts name
     puts "established at year #{year}"
     puts "number of beers #{beers.count}"
@@ -19,5 +17,24 @@ class Brewery < ActiveRecord::Base
     self.year = 2014
     puts "changed year to #{year}"
   end
+
+  def validate_year_format
+    if !year.integer?
+        errors.add(:year, "Year must be a 4-digit integer")
+    end
+  end
+
+  def validate_end_year
+    if year > Time.new.year
+      errors.add(:year, "Year cannot be set higher than the current year")
+    end
+  end
+
+  def validate_start_year
+    if year < 1042
+      errors.add(:year, "Year cannot be set lower than 1042")
+    end
+  end
+
 end
 
